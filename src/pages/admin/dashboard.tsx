ca11515bdd37a714aa6055340db82422b1547bac
geofannyher <geofannyher@gmail.co>
2024-07-17 11:06:59 +0700

    udpate

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -17,7 +17,7 @@ const DashboardAdmin = () => {
 
   const getSes = async () => {
     const res = await getIdSession();
-    setId(res?.data?.uuid);
+    setId(res?.data?.uuid_ikn);
   };
 
   const switchID = async () => {
