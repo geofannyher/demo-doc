d43281c21d651e90b29548123c6e9742946dbbb8
geofannyher <geofannyher@gmail.co>
2024-08-23 11:43:55 +0700

    update

--- src/pages/video.tsx
+++ src/pages/video.tsx
@@ -1,11 +1,18 @@
+import { useRef } from "react";
+
 const VideoPlayerr = () => {
+  const videoRef = useRef(null);
+  const handleChange = () => {
+    console.log(videoRef.current);
+  };
   return (
     <div>
       <video autoPlay muted controls>
-        <source
+        <iframe
           src="https://drive.google.com/uc?id=11wGtvBt5LPmM_9y_aidE62R87V6PXWsx"
-          type="video/mp4"
-        />
+          ref={videoRef}
+          onTimeUpdate={handleChange}
+        ></iframe>
         Your browser does not support the video tag.
       </video>
     </div>
