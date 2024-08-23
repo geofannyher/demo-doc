0ad1e00cb6cd04c9cb9d90ab1373c0a3efa54510
geofannyher <geofannyher@gmail.co>
2024-08-23 11:30:16 +0700

    update

--- src/pages/video.tsx
+++ src/pages/video.tsx
@@ -3,7 +3,7 @@ const VideoPlayerr = () => {
     <div>
       <video autoPlay muted controls>
         <source
-          src="https://drive.google.com/file/d/11wGtvBt5LPmM_9y_aidE62R87V6PXWsx/view?usp=sharing"
+          src="https://drive.google.com/thumbnail?id=11wGtvBt5LPmM_9y_aidE62R87V6PXWsx&sz=w1000"
           type="video/mp4"
         />
         Your browser does not support the video tag.
